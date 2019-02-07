06f3288105ece12a1784a6996b922b5b1a120bd7
root <root@chaos.home>
2019-02-07 13:44:32 +1000

    travis.yml

--- exceptor.py
+++ exceptor.py
@@ -7,8 +7,3 @@ class Exceptor:
     except Exception as e:
         print("Error! in Exceptor.catchit(): " + str(e))
         exit(1)
-<<<<<<< HEAD
-
-
-=======
->>>>>>> cb2853016e5d6bcd97b9e8528b1acd2d34d606b8
